4cc6127e5d24ffa200ab224a232ebfd9562c867c
Gabe Gomez <gag014@bucknell.edu>
2017-04-24 01:21:25 -0400

    Final Project, Better scaling and method to add planets and their path to a root

--- src/SolarSystem/SolarSystem.java
+++ src/SolarSystem/SolarSystem.java
@@ -6,38 +6,51 @@
 package SolarSystem;
 
 import java.util.ArrayList;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Gabe
  */
 public class SolarSystem {
-    
+
     private ArrayList<Planet> solarSystem;
     String name;
-    
+
     public SolarSystem(String name) {
         solarSystem = new ArrayList<>();
         this.name = name;
     }
-    
+
     public void addPlanet(Planet newPlanet) {
         solarSystem.add(newPlanet);
     }
-    
+
     public Planet planetSelector(int index) {
         return solarSystem.get(index);
     }
-    
+
     public int planetsInSystem() {
         return solarSystem.size();
     }
-    
+
     public String getName() {
         return this.name;
     }
-    
+
     public void setName(String newName) {
         this.name = newName;
+    }
+
+    public BorderPane setSystemToRoot(BorderPane root, Stage primaryStage) {
+
+        for (int i = 0; i < planetsInSystem(); i++) {
+            solarSystem.get(i).setPath(primaryStage);
+            root.getChildren().add(solarSystem.get(i).getPlanet());
+            root.getChildren().add(solarSystem.get(i).getPath());
+        }
+
+        return root;
     }
 }
